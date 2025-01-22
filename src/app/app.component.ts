6576ba1f76ed37329d1aa1c90484678b47d3d183
LukasPegasus <lucas.souza@precojusto.app.br>
2025-01-22 17:18:13 -0400

    (feat): creating new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './modules/portfolio/components/header/header.component';
+import { KnowledgeComponent } from './modules/portfolio/components/knowledge/knowledge.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, HeaderComponent, KnowledgeComponent],
+  template: ` <app-header></app-header>
+    <app-knowledge></app-knowledge>`,
 })
 export class AppComponent {
   title = 'portfolio-angular';
